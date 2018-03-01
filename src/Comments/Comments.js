8edbbb72f52fd5af1ae88714e9e069e0e3039437
mvcbotelho <marcus.itec@gmail.com>
2018-02-28 21:43:41 -0300

    Create a new component from Comment

--- src/Comments/Comments.js
+++ src/Comments/Comments.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 
 export default class Comments extends Component {
+  renderComment(key, comment) {
+    return (
+      <p key={key}>{comment.comment}</p>
+    )
+  }
   render() {
     return (
       <div>
         <h2>Comentários aqui!!!!</h2>
-        <div>{JSON.stringify(this.props.comments)}</div>
+        <div>{Object.keys(this.props.comments)
+          .map(key => this.renderComment(key, this.props.comments[key]))}
+        </div>
       </div>
     )
   }
