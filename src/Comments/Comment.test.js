86729e381159af821a9710b895bc8a5c46d0e9b1
mvcbotelho <marcus.itec@gmail.com>
2018-03-02 21:50:17 -0300

    Add test in Comment

--- src/Comments/Comment.test.js
+++ src/Comments/Comment.test.js
@@ -4,8 +4,28 @@ import Comment from './Comment'
 import { shallow, mount, render } from 'enzyme'
 
 describe('<Comment />', () => {
+  const comment = {
+    comment: 'test'
+  }
   it('renders without crashing', () => {
-    const wrapper = shallow(<Comment />)
+    const wrapper = shallow(<Comment comment={comment} />)
     expect(wrapper.length).toBe(1)
+  })
+  it('have a class .card', () => {
+    const wrapper = shallow(<Comment comment={comment} />)
+    expect(wrapper.is('.card')).toBe(true)
+  })
+  it('have a class .bg-light', () => {
+    const wrapper = shallow(<Comment comment={comment} />)
+    expect(wrapper.is('.bg-light')).toBe(true)
+  })
+  it('have a class .card-body', () => {
+    const wrapper = shallow(<Comment comment={comment} />)
+    expect(wrapper.is('.card-body')).toBe(true)
+  })
+  it('Text in comment.comment === test', () => {
+    const wrapper = shallow(<Comment comment={comment} />)
+    console.log(wrapper.text())
+    expect(wrapper.text()).toBe(comment.comment)
   })
 })
